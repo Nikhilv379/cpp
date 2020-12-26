ace1cfc1c116227ec662be33b066e5483d1edab6
Nikhilv379 <76606942+Nikhilv379@users.noreply.github.com>
2020-12-26 17:02:28 +0530

    Create Addition_of_two_numbers.cpp

---
+++ Addition_of_two_numbers.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a,b,sum;
+        cout<<"Enter the value for a : ";
+         cin>>a;
+        cout<<"Enter the value for b : ";
+         cin>>b;
+        sum=a+b;
+        cout<<"The sum of a & b is : "<<sum;
+    return 0;
+}
